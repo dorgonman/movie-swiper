c937f06906cc6a63936458c39577e4849b589247
dorgon.chang <dorgonman@hotmail.com>
2019-11-14 18:04:05 +0800

    [Fix] Don't call react-native bundle because it already generated in build/generated/assets

--- android/ci/install_release_apk.sh
+++ android/ci/install_release_apk.sh
@@ -2,17 +2,17 @@
 set -e
 ushd ..
 
-    pushd ..
-        cmd="react-native bundle --platform android --dev false  \
-        --entry-file index.js \
-        --bundle-output android/app/src/main/assets/index.android.bundle --assets-dest android/app/src/main/res \
-        "
-        echo $cmd
-        eval $cmd
-    popd
+    # pushd ..
+    #     cmd="react-native bundle --platform android --dev false  \
+    #     --entry-file index.js \
+    #     --bundle-output android/app/src/main/assets/index.android.bundle --assets-dest android/app/src/main/res \
+    #     "
+    #     echo $cmd
+    #     eval $cmd
+    # popd
 
-    rm -rf ./app/src/main/res/drawable-*
-    rm -rf ./app/src/main/res/raw
+    # rm -rf ./app/src/main/res/drawable-*
+    # rm -rf ./app/src/main/res/raw
 
 
     ./gradlew installRelease
