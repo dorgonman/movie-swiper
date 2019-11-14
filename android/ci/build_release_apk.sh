c3c35df2f9abd840a42f0f38e9bd22c5770eee67
dorgon.chang <dorgonman@hotmail.com>
2019-11-14 11:37:20 +0800

    [ci] add build script and fix bundle error

---
+++ android/ci/build_release_apk.sh
@@ -0,0 +1,17 @@
+pushd ..
+
+    pushd ..
+        cmd="react-native bundle --platform android --dev false  \
+        --entry-file index.js \
+        --bundle-output android/app/src/main/assets/index.android.bundle --assets-dest android/app/src/main/res \
+        "
+        echo $cmd
+        eval $cmd
+    popd
+
+    rm -rf ./app/src/main/res/drawable-*
+    rm -rf ./app/src/main/res/raw
+
+
+    ./gradlew assembleRelease
+popd
